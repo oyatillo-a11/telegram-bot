07815f46fddfbd1cdb61afbd529602f0efc9ed68
oyatillo-a11 <oyatillox79@gmail.com>
2026-06-05 20:36:14 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,38 @@
-import asyncio
 import os
+import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
 from aiohttp import web
-TOKEN = "8678456257:AAGPBLpsSyuArYiSPgGGuY8S4a4E"
-
+TOKEN = "8678456257:AAGPBLpsSyuArYiSPGgGUy8S4a4EnfuV"
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
-
 @dp.message(CommandStart())
 async def start_command(message: types.Message):
-    await message.answer(f"Salom {message.from_user.full_name}! SlaydTop botiga xush kelibsiz.")
+    await message.answer(f"Salom {message.from_user.full_name}!")
 
 @dp.message()
 async def echo_message(message: types.Message):
     await message.answer(f"Siz yozdingiz: {message.text}")
+
 async def handle(request):
     return web.Response(text="Bot is running alive!")
 
 async def main():
     app = web.Application()
     app.router.add_get("/", handle)
-    
+
     port = int(os.environ.get("PORT", 10000))
     runner = web.AppRunner(app)
     await runner.setup()
-    site = web.TCPSite(runner, "0.0.0.0", 
+    site = web.TCPSite(runner, "0.0.0.0", port)
     await site.start()
+    
     print(f"Veb-server {port}-portda ishga tushdi")
+    
     try:
         await dp.start_polling(bot)
     finally:
         await bot.session.close()
 
 if __name__ == "__main__":
-    import asyncio
     asyncio.run(main())
-
-    
